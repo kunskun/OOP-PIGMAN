9c3c5c423ce8036118ddf069309a089b32fa76a9
kuns019 <56548175+kuns019@users.noreply.github.com>
2020-12-09 03:26:57 +0700

    update

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -162,7 +162,7 @@ public class GameController extends Canvas implements Runnable, KeyListener, Mou
             //screen = new SetScreen("res/load/jok.png");
             g.drawImage(TextureModel.imgWin, 0, 0, 640, 480, null);
             g.setFont(new Font("FC SaveSpace Rounded", Font.BOLD, 24));
-            g.drawString(tm.getLastTime(), 350, 245);
+            g.drawString(new GetScore().getScore(), 350, 245);
             g.setFont(new Font("FC SaveSpace Rounded", Font.BOLD, 24));
             g.drawString(tm.getLastTime(), 340, 279);
 
